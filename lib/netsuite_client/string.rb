bd8593464c01b09380a93e1fd1f2dcf08a028ad8
Rob Kaufman <rob@notch8.com>
2011-12-21 14:29:00 -0800

    perge some active support code

--- lib/netsuite_client/string.rb
+++ lib/netsuite_client/string.rb
@@ -1,31 +1,4 @@
-# Borrowed from active support.
-# Ruby 1.9 introduces an inherit argument for Module#const_get and
-# #const_defined? and changes their default behavior.
-
 class String
-  if Module.method(:const_get).arity == 1
-    def constantize(camel_cased_word)
-      names = camel_cased_word.split('::')
-      names.shift if names.empty? || names.first.empty?
-
-      constant = Object
-      names.each do |name|
-        constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
-      end
-      constant
-    end
-  else
-    def constantize(camel_cased_word) #:nodoc:
-      names = camel_cased_word.split('::')
-      names.shift if names.empty? || names.first.empty?
-
-      constant = Object
-      names.each do |name|
-        constant = constant.const_defined?(name, false) ? constant.const_get(name) : constant.const_missing(name)
-      end
-      constant
-    end
-  end
 
   def self.random_string(len = 8)
     ar = 'abcdefgijhklmnopqrstwxyz0123456789'.split('')
